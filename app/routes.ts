3b3054a631395eaa718f0b89f3000106ce0b7fba
callan321 <callan.mitchell11@gmail.com>
2025-03-19 21:41:59 +1100

    Fix case sensitivity issue

--- app/routes.ts
+++ app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 export default [
     layout("layouts/RootLayout.tsx",
         [
-            index("routes/home.tsx"),
+            index("routes/Home.tsx"),
             route("About", "routes/About.tsx"),
             route("Heather", "routes/Heather.tsx"),
         ])
