0cd9989949ea5986f3b78edff8dccfd96df7c0b9
Callan <callan.mitchell11@gmail.com>
2025-06-19 15:02:35 +1000

    resolve useContext null error by enabling unstable_optimizeDeps and moved meta tags into recommended react router 7 meta functions

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  // Add this future flag to fix the useContext null error
+  future: {
+    unstable_optimizeDeps: true,
+  },
 } satisfies Config;
